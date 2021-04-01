5e598bfc959cffbbbb37eed9991e015bf06ad176
Ilya Kiyanov <ikiyanov@yahoo.com>
2021-04-02 01:45:10 +0300

    changes token file to env, changes file structure

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var Discord = require('discord.js');
-var config = require('./config.json');
-var client = new Discord.Client();
-client.login(config.BOT_TOKEN);
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const Discord = require('discord.js');
-const config = require('./config.json');
 
 const client = new Discord.Client();
 
-client.login(config.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
