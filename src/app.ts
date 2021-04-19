5e59b3de2209c86360482189a686ae79091ce1ec
Ilya Kiyanov <ikiyanov@yahoo.com>
2021-04-20 00:00:38 +0300

    configs changes, adds service to make http call every 3 minutes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,26 @@
 import dotenv from 'dotenv';
-import {start} from './start'
+import express from 'express';
+import http, {RequestOptions} from 'http';
+import {start} from './start';
+
+const expressService = express();
+
+expressService.get('/', () => {
+  console.log('PING SERVICE:', new Date());
+});
+
+expressService.listen(8081);
+
+const reqOptions: RequestOptions = {
+  host: '127.0.0.1',
+  port: 8081,
+  path: '/'
+};
+
+setInterval(() => {
+  const req = http.request(reqOptions)
+  req.end();
+}, 180000)
 
 dotenv.config()
 start()
